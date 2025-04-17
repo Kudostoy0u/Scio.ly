9e3847aa3f9500d0a9161bb027d0b36db31ea67f
Kundan Baliga <kudosbeluga@gmail.com>
2025-04-17 11:37:06 -0500

    Update api.tsx

--- frontend/science-olympiad-practice/src/app/api.tsx
+++ frontend/science-olympiad-practice/src/app/api.tsx
@@ -1,5 +1,5 @@
 const api = {
-    api: "https://gist.githubusercontent.com/Kudostoy0u/35964bbd2c4d9dcd77a6304bea6c3de9/raw/7401fd339b205aecb92789d59dbc722baf39b4ce/final.json",
+    api: "https://gist.githubusercontent.com/Kudostoy0u/b428bcce3cf270673786702ea934a0ba/raw/7d2c849590d8fd408aadec3c085e6957886ba098/final.json",
     arr: JSON.parse(process.env.NEXT_PUBLIC_API_KEYS || "[]"),
 };
 
